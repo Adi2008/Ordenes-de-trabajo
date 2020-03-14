ccca0879eedba947c7af082abe2115a25eb4ed92
Adilia <adilia.arias@unah.hn>
2020-03-13 20:42:04 -0700

    subiendo calculos de ordenes

--- OrdenesDeTrabajo/OrdenesdeTrabajo.WebAdmin/Controllers/OrdenesDetalleController.cs
+++ OrdenesDeTrabajo/OrdenesdeTrabajo.WebAdmin/Controllers/OrdenesDetalleController.cs
@@ -60,5 +60,21 @@ namespace OrdenesdeTrabajo.WebAdmin.Controllers
 
             return View(ordenDetalle);
         }
+
+        public ActionResult Eliminar(int id)
+        {
+            var ordenDetalle = _ordenBL.ObtenerOrdenDetallePorId(id);
+
+            return View(ordenDetalle);
+        }
+
+        [HttpPost]
+        public ActionResult Eliminar(OrdenDetalle ordenDetalle)
+        {
+            _ordenBL.EliminarOrdenDetalle(ordenDetalle.Id);
+
+            return RedirectToAction("Index", new { id = ordenDetalle.OrdenId });
+        }
+
     }
 }
